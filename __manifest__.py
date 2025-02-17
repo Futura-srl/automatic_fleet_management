947a8c58554921c1025e06c0c47e5e523dc7f6f1
marcelloGalloStesi <gallo.m@stesi.consulting>
2025-02-17 16:49:29 +0100

    fix temporaneo migrazione database

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     'data': [
         # Settaggi per accesso ai contenuti
         'data/ir.model.access.csv',
-        'data/data.xml',
+        #'data/data.xml',
         # Caricamento delle view,
         'view/fleet_vehicle_log_contract_update.xml',
     ],
